63f498c4b6fbdc282dd1b07ff7f441478798190c
Vinícius Cordeiro Pereira <62481046+Vini-cp@users.noreply.github.com>
2021-11-15 22:49:22 -0300

    Create map utils file

---
+++ src/ft_map_utils.c
@@ -0,0 +1,13 @@
+#include "../so_long.h"
+
+int	ft_check_file_extension(char *file_name)
+{
+	char *extension;
+
+	extension = (char *) &file_name[(ft_strlen(file_name) - 4)];
+	if (ft_strncmp(extension, ".ber", 4) == 0)
+  {   
+	  	return (SUCCESS_EXIT);
+  }
+	return (FAILURE_EXIT);
+}
