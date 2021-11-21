41d9f35dd150a393d737e24b183cb50f462d88e1
Vinícius Cordeiro Pereira <62481046+Vini-cp@users.noreply.github.com>
2021-11-20 22:54:11 -0300

    Update header

--- so_long.h
+++ so_long.h
@@ -10,11 +10,19 @@
 # define SUCCESS_EXIT 1
 # define FAILURE_EXIT 0
 
+# define ESC 53
+# define UP 13
+# define DOWN 1
+# define LEFT 0
+# define RIGHT 2
+
 # define PLAYER "./assets/player.xpm"
 # define WALL "./assets/wall.xpm"
 # define BACKGROUND "./assets/background.xpm"
 # define EXIT "./assets/exit.xpm"
 # define COLLECTIBLE "./assets/collectible.xpm"
+
+# define ORANGE 0xFE6500
 
 typedef struct win_render {
 	void	*mlx;
@@ -29,17 +37,25 @@ typedef struct game_set
 	int		player_position_x;
 	int		player_position_y;
   int   player_movements;
+  int   no_collectibles;
+  int   no_exits;
+  int   game_won;
   void	*player;
   void	*wall;
   void	*background;
 	void	*exit;
 	void	*collectible;
+  t_win_render *game_window;
 }	t_game_set;
 
-int ft_game_loop(t_game_set *game_set)
+int ft_game_loop(t_game_set *game_set);
 int	ft_get_map(int argc, char *argv[], t_game_set **game_set);
 int	ft_check_file_extension(char *file_name);
+int	ft_get_info_from_map(t_game_set ***game_set);
+int	ft_key_hook(int keycode, t_game_set **game_set);
+int	ft_exit_hook(t_game_set **game_set);
 void	ft_build_images(t_game_set **game_set, t_win_render **game_window);
 void	ft_render(t_game_set *game_set, t_win_render *game_window);
+void	ft_free(t_game_set *game_set, t_win_render *game_window);
 
 #endif
