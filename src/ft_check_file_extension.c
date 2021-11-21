083f46a53fb031c6ba47a902ddf51b9d3d5e9481
Vinícius Cordeiro Pereira <62481046+Vini-cp@users.noreply.github.com>
2021-11-21 10:23:57 -0300

    Rename and norminette check_file_ext file

--- src/ft_check_file_extension.c
+++ src/ft_check_file_extension.c
@@ -2,12 +2,12 @@
 
 int	ft_check_file_extension(char *file_name)
 {
-	char *extension;
+	char	*extension;
 
 	extension = (char *) &file_name[(ft_strlen(file_name) - 4)];
 	if (ft_strncmp(extension, ".ber", 4) == 0)
-  {   
-	  	return (SUCCESS_EXIT);
-  }
+	{
+		return (SUCCESS_EXIT);
+	}
 	return (FAILURE_EXIT);
 }
