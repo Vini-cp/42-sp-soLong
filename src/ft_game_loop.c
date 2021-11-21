5e09e83775829d8ab2a33cdf0aae11c10aac6b3a
Vinícius Cordeiro Pereira <62481046+Vini-cp@users.noreply.github.com>
2021-11-20 23:14:49 -0300

    Norminette loop file

--- src/ft_game_loop.c
+++ src/ft_game_loop.c
@@ -1,20 +1,25 @@
 #include "../so_long.h"
 
-int ft_game_loop(t_game_set *game_set)
+int	ft_game_loop(t_game_set *game_set)
 {
-  t_win_render *game_window;
+	int	len;
+	int	hgt;
+	t_win_render	*game_window;
 
-  game_window = malloc(sizeof(game_window));
-
-  if ((game_window->mlx = mlx_init()) == NULL)
-    return (FAILURE_EXIT);
-  if ((game_window->win = mlx_new_window(game_window->mlx, TILE_WIDTH * game_set->map_length, TILE_WIDTH * game_set->map_height, WINDOW_NAME)) == NULL)
-    return (FAILURE_EXIT);
-  game_set->game_window = game_window;
-  ft_build_images(&game_set, &game_window);
-  ft_render(game_set, game_window);
+	len = TILE_WIDTH * game_set->map_length;
+	hgt = TILE_WIDTH * game_set->map_height;
+	game_window = malloc(sizeof(game_window));
+	game_window->mlx = mlx_init();
+	if (game_window->mlx == NULL)
+		return (FAILURE_EXIT);
+	game_window->win = mlx_new_window(game_window->mlx, len, hgt, WINDOW_NAME);
+	if (game_window->win == NULL)
+		return (FAILURE_EXIT);
+	game_set->game_window = game_window;
+	ft_build_images(&game_set, &game_window);
+	ft_render(game_set, game_window);
 	mlx_key_hook(game_window->win, ft_key_hook, &game_set);
 	mlx_hook(game_window->win, 17, 0, ft_exit_hook, &game_set);
 	mlx_loop(game_window->mlx);
-  return (SUCCESS_EXIT);
+	return (SUCCESS_EXIT);
 }
