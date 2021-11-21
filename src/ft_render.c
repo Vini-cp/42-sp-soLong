b82f0496367f497db25b86b6fb5657f7455a1187
Vinícius Cordeiro Pereira <62481046+Vini-cp@users.noreply.github.com>
2021-11-20 22:53:32 -0300

    Add a exit render function

--- src/ft_render.c
+++ src/ft_render.c
@@ -5,13 +5,17 @@ static void	ft_put_image(t_win_render *game_window, void *img, int x, int y)
 	mlx_put_image_to_window(game_window->mlx, game_window->win, img, x, y);
 }
 
-void	ft_render(t_game_set *game_set, t_win_render *game_window)
+static void ft_render_exit(t_game_set *game_set, t_win_render *game_window)
 {
-	int		x_pos;
+  mlx_string_put(game_window->mlx, game_window->win, (game_set->map_length / 2) * TILE_WIDTH, (game_set->map_height / 2) * TILE_WIDTH, ORANGE, "You win!");
+}
+
+static void ft_render_game(t_game_set *game_set, t_win_render *game_window)
+{
+  int		x_pos;
 	int		y_pos;
   char  map_position;
 
-  mlx_clear_window(game_window->mlx, game_window->win);
   y_pos = 0;
 	while (y_pos < game_set->map_height)
 	{
@@ -28,8 +32,22 @@ void	ft_render(t_game_set *game_set, t_win_render *game_window)
         ft_put_image(game_window, game_set->exit, x_pos * TILE_WIDTH, y_pos * TILE_WIDTH);
       else if (map_position == 'P')
         ft_put_image(game_window, game_set->player, x_pos * TILE_WIDTH, y_pos * TILE_WIDTH);
+      else if (map_position == 'S')
+      {
+        ft_put_image(game_window, game_set->exit, x_pos * TILE_WIDTH, y_pos * TILE_WIDTH);
+        ft_put_image(game_window, game_set->player, x_pos * TILE_WIDTH, y_pos * TILE_WIDTH);
+      }
 			x_pos++;
 		}
 		y_pos++;
 	}
+}
+
+void	ft_render(t_game_set *game_set, t_win_render *game_window)
+{
+  mlx_clear_window(game_window->mlx, game_window->win);
+  if (game_set->game_won == 1)
+    ft_render_exit(game_set, game_window);
+  else
+    ft_render_game(game_set, game_window);
 }
