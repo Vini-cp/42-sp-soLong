6c2ed17cb63e8f18f0acc2ba5b262bad1da70c90
Vinícius Cordeiro Pereira <62481046+Vini-cp@users.noreply.github.com>
2021-11-20 22:53:53 -0300

    Add all functions to loop

--- src/ft_game_loop.c
+++ src/ft_game_loop.c
@@ -10,8 +10,11 @@ int ft_game_loop(t_game_set *game_set)
     return (FAILURE_EXIT);
   if ((game_window->win = mlx_new_window(game_window->mlx, TILE_WIDTH * game_set->map_length, TILE_WIDTH * game_set->map_height, WINDOW_NAME)) == NULL)
     return (FAILURE_EXIT);
+  game_set->game_window = game_window;
   ft_build_images(&game_set, &game_window);
   ft_render(game_set, game_window);
-	ft_key_hook(-1, &vars);
+	mlx_key_hook(game_window->win, ft_key_hook, &game_set);
+	mlx_hook(game_window->win, 17, 0, ft_exit_hook, &game_set);
+	mlx_loop(game_window->mlx);
   return (SUCCESS_EXIT);
 }
