57cceaeb42b48ce561ce5d88680ca9705476f072
Vinícius Cordeiro Pereira <62481046+Vini-cp@users.noreply.github.com>
2021-11-21 10:18:46 -0300

    Norminette get_info_from_map file

--- src/ft_get_info_from_map.c
+++ src/ft_get_info_from_map.c
@@ -6,15 +6,20 @@ static void	ft_set_player_position(t_game_set ****game_set, int x, int y)
 	(***game_set)->player_position_y = y;
 }
 
+static void	ft_set_initial_variables(t_game_set ****game_set)
+{
+	(***game_set)->no_exits = 0;
+	(***game_set)->no_collectibles = 0;
+	(***game_set)->player_movements = 0;
+	(***game_set)->game_won = 0;
+}
+
 int	ft_get_info_from_map(t_game_set ***game_set)
 {
 	int	x_pos;
 	int	y_pos;
 
-	(**game_set)->no_exits = 0;
-	(**game_set)->no_collectibles = 0;
-  (**game_set)->player_movements = 0;
-  (**game_set)->game_won = 0;
+	ft_set_initial_variables(&game_set);
 	y_pos = 0;
 	while (y_pos < (**game_set)->map_height)
 	{
@@ -31,7 +36,9 @@ int	ft_get_info_from_map(t_game_set ***game_set)
 		}
 		y_pos++;
 	}
-	if ((**game_set)->no_collectibles == 0 || (**game_set)->no_exits == 0 || (**game_set)->player_position_x == 0)
+	if ((**game_set)->no_collectibles == 0 || (**game_set)->no_exits == 0
+		|| (**game_set)->player_position_x == 0
+		|| (**game_set)->player_position_y == 0)
 		return (FAILURE_EXIT);
 	return (SUCCESS_EXIT);
 }
