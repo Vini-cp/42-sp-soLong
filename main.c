d1f8326bfe044041ea521d461b454fdba6406b2e
Vinícius Cordeiro Pereira <62481046+Vini-cp@users.noreply.github.com>
2021-11-20 22:55:26 -0300

    Norminette main file

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include "so_long.h"
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-  t_game_set *game_set;
+	t_game_set	*game_set;
 
-  if (!ft_get_map(argc, argv, &game_set))
-    return (FAILURE_EXIT);
-  if (!ft_game_loop(game_set))
-    return (FAILURE_EXIT);
-  return (SUCCESS_EXIT);
+	if (!ft_get_map(argc, argv, &game_set))
+		return (FAILURE_EXIT);
+	if (!ft_game_loop(game_set))
+		return (FAILURE_EXIT);
+	return (SUCCESS_EXIT);
 }
