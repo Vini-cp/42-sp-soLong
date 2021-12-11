df0116bf3a44483f9a1a80fc2fc9a22564f8733c
Vinícius Cordeiro Pereira <62481046+Vini-cp@users.noreply.github.com>
2021-12-11 18:08:38 -0300

    Destroy window when exit hook

--- src/ft_exit_hook.c
+++ src/ft_exit_hook.c
@@ -2,6 +2,8 @@
 
 int	ft_exit_hook(t_game_set **game_set)
 {
+	mlx_destroy_window(((*game_set)->game_window)->mlx,
+			((*game_set)->game_window)->win);
 	ft_free(game_set, (*game_set)->game_window);
 	exit(0);
 }
