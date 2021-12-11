917f254bc13a83ce2fa880a60169793d0aad53eb
Vinícius Cordeiro Pereira <62481046+Vini-cp@users.noreply.github.com>
2021-12-11 18:12:48 -0300

    Free memory when something breaks

--- main.c
+++ main.c
@@ -4,9 +4,10 @@ int	main(int argc, char *argv[])
 {
 	t_game_set	*game_set;
 
-	if (!ft_get_map(argc, argv, &game_set))
+	if (!ft_get_map(argc, argv, &game_set) || !ft_game_loop(&game_set))
+	{
+		ft_free(&game_set, game_set->game_window);
 		return (FAILURE_EXIT);
-	if (!ft_game_loop(&game_set))
-		return (FAILURE_EXIT);
+	}
 	return (SUCCESS_EXIT);
 }
