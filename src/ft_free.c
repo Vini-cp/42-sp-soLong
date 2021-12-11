77bf2b6a010f8f61773fbf0a26cb6abf7fd7f87f
Vinícius Cordeiro Pereira <62481046+Vini-cp@users.noreply.github.com>
2021-12-11 18:05:35 -0300

    Free up memory correctly

--- src/ft_free.c
+++ src/ft_free.c
@@ -1,23 +1,23 @@
 #include "../so_long.h"
 
-static void	ft_free_map(char **map, int size)
+static void	ft_free_map(char ***map, int size)
 {
 	int	i;
 
 	i = 0;
 	while (i < size)
 	{
-		free(map[i]);
-		map[i] = NULL;
+		free(*map[i]);
+		*map[i] = NULL;
 		i++;
 	}
 	free(map);
+	map = NULL;
 }
 
 void	ft_free(t_game_set **game_set, t_win_render *game_window)
 {
-	ft_free_map((*game_set)->map, (*game_set)->map_height);
-	(*game_set)->map = NULL;
+	ft_free_map(&(*game_set)->map, (*game_set)->map_height);
 	free(*game_set);
 	free(game_window);
 }
