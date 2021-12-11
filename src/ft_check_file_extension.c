09be845ecec61bbd20cc48c6deab1c07239d9408
Vinícius Cordeiro Pereira <62481046+Vini-cp@users.noreply.github.com>
2021-12-11 18:10:28 -0300

    Remove unnecessary curly brackets

--- src/ft_check_file_extension.c
+++ src/ft_check_file_extension.c
@@ -6,8 +6,6 @@ int	ft_check_file_extension(char *file_name)
 
 	extension = (char *) &file_name[(ft_strlen(file_name) - 4)];
 	if (ft_strncmp(extension, ".ber", 4) == 0)
-	{
 		return (SUCCESS_EXIT);
-	}
 	return (FAILURE_EXIT);
 }
