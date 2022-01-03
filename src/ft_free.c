a645251a61658a5b2b243a0034c30da9d1eaaf26
Vinícius Cordeiro Pereira <62481046+Vini-cp@users.noreply.github.com>
2022-01-02 21:17:33 -0300

    Fix free function

--- src/ft_free.c
+++ src/ft_free.c
@@ -1,23 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_free.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vcordeir <vcordeir@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/02 21:13:29 by vcordeir          #+#    #+#             */
+/*   Updated: 2022/01/02 21:14:10 by vcordeir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../so_long.h"
 
-static void	ft_free_map(char ***map, int size)
+static void	ft_free_map(char **map, int map_len)
 {
 	int	i;
 
 	i = 0;
-	while (i < size)
+	while (i < map_len)
 	{
-		free(*map[i]);
-		*map[i] = NULL;
+		free(map[i]);
 		i++;
 	}
 	free(map);
-	map = NULL;
 }
 
 void	ft_free(t_game_set **game_set, t_win_render *game_window)
 {
-	ft_free_map(&(*game_set)->map, (*game_set)->map_height);
+	ft_free_map((*game_set)->map, (*game_set)->map_height);
 	free(*game_set);
 	free(game_window);
 }
